43606de8b22626fd30838cf146b217aba973ea47
Clifford Ettah <sircliffjoe11@gmail.com>
2021-07-18 01:10:20 +0100

    disabled devise confirmation

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,9 +1,9 @@
-class ConfirmationsController < Devise::ConfirmationsController
+# class ConfirmationsController < Devise::ConfirmationsController
 
-  private
-    def after_confirmation_path_for(resource_name, resource)
-      sign_in(resource)
-      dashboard_path(current_user.full_name)
-      new_user_session_path
-    end 
-end
+#   private
+#     def after_confirmation_path_for(resource_name, resource)
+#       sign_in(resource)
+#       dashboard_path(current_user.full_name)
+#       new_user_session_path
+#     end 
+# end

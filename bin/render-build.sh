ed8da7d6a9bf4bbab2048bac8d541a5f26bf5af6
Ettah Clifford <sircliffjoe11@gmail.com>
2023-02-22 00:03:55 +0100

    added media queries for dashboard and profile

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 # exit on error
+# set -o errexit
+
+# bundle install
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+# bundle exec rails db:migrate
+# bundle exec rails db:seed
+
 set -o errexit
 
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-# bundle exec rails db:seed
+
+# Clear the Category table
+bundle exec rails runner 'Category.delete_all'
+
+# Re-seed the database
+bundle exec rails db:seed

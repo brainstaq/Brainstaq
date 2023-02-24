50b34a64b3b8fda4f3b2f77c1c9e0923ffe17b92
Ettah Clifford <sircliffjoe11@gmail.com>
2023-02-24 23:07:48 +0100

    more mobile styles

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -16,7 +16,7 @@ bundle exec rails assets:clean
 bundle exec rails db:migrate
 
 # Clear the Category table
-bundle exec rails runner 'Category.delete_all'
+# bundle exec rails runner 'Category.delete_all'
 
 # Re-seed the database
-bundle exec rails db:seed
+# bundle exec rails db:seed

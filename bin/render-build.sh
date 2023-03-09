b78fe0120e0b1dfbab4c43bb8fe4b70e2baa9240
Ettah Clifford <sircliffjoe11@gmail.com>
2023-03-09 11:39:02 +0100

    updated build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,15 +8,36 @@
 # bundle exec rails db:migrate
 # bundle exec rails db:seed
 
-set -o errexit
+# set -o errexit
 
-bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+# bundle install
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+# bundle exec rails db:migrate
 
 # Clear the Category table
 # bundle exec rails runner 'Category.delete_all'
 
 # Re-seed the database
 # bundle exec rails db:seed
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# check if RAILS_ENV is not set
+if [ -z "${RAILS_ENV}" ]
+then
+  export RAILS_ENV=production
+fi
+
+# install dependencies and run migrations
+bundle install
+bundle exec rails db:migrate
+
+# clean and precompile assets
+bundle exec rails assets:clean
+bundle exec rails assets:precompile
+
+# seed the database
+bundle exec rails db:seed
